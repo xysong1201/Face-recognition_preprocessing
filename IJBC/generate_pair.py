6356b8fe3c4d939c6e459c2d50b68b39f70e110f
xiaoyu SONG <44894406+xysong1201@users.noreply.github.com>
2019-10-21 11:27:03 +0800

    Update generate_pair.py

--- IJBC/generate_pair.py
+++ IJBC/generate_pair.py
@@ -1,19 +1,11 @@
 import pandas as pd
 import os
 
-
-
 csv_path = '/media/xysong/Disk_2/protocols/ijbc_11_covariate_probe_reference.csv'
 csv_pair_path = '/media/xysong/Disk_2/protocols/ijbc_11_covariate_matches.csv'
 
-
 info_cordinate = pd.read_csv(csv_path,usecols = ['TEMPLATE_ID','SUBJECT_ID'])
-
-
-
 pair_cordinate = pd.read_csv(csv_pair_path,names=["TEMPLATE_ID", "TEMPLATE_ID_2"])
-pair_cordinate
-
 
 template_dic = {}
 template_index = {}
@@ -23,10 +15,7 @@ for i in range(0,140739):
     subject_id = info_cordinate['SUBJECT_ID'][i]
     template_dic[template_id] = subject_id
     template_index[template_id] = i+1
-
-
 f = open('covariate_pair_xysong.txt', 'w+')
-
 
 label = 0
 label_1_count = 0
@@ -36,8 +25,7 @@ for j in range(0,47404001):
     tem_2 = pair_cordinate['TEMPLATE_ID_2'][j]
     if template_dic[tem_1] == template_dic[tem_2]:
         label = 1
-        label_1_count += 1
-        
+        label_1_count += 1        
     else:
         label = 0
         label_0_count +=1
@@ -46,4 +34,3 @@ print('label 1 number:' + str(label_1_count))
 print('label 0 number:' + str(label_0_count))
 
 f.close()
-
